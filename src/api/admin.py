f533e9eda95cbb0452a40a382a1d93261cb13cb7
BPond4 <bpond@calpoly.edu>
2024-04-12 10:32:25 -0700

    indentation

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/admin",
@@ -14,5 +16,8 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
-    return "OK"
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = :new_ml, num_green_potions = :new_potions, gold =:new_gold"),
+        {"new_ml": 0, "new_potions": 0, "new_gold": 100})
+    return result
 
--- src/api/barrels.py
+++ src/api/barrels.py
@@ -53,16 +53,17 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     
     if(green_potions<10):
         for barrel in wholesale_catalog:
-            if barrel.potion_type[1] == 100:
+            if barrel.potion_type[1]==100:
                 sku = barrel.sku
-        return [
-            {
-                "sku": sku,
-                "quantity": 1,
-            }
-        ]
-    else:
-        return[
-        ]
+                return [
+                    {
+                        "sku": sku,
+                        "quantity": 1,
+                    }
+                ]
+            else:
+                return[
+                
+                ]
     
 
