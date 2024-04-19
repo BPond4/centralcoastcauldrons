fbbbfd5da783c9b16cdfd689747e11ecc039509d
BPond4 <bpond@calpoly.edu>
2024-04-19 11:08:02 -0700

    fixed carts

--- src/api/carts.py
+++ src/api/carts.py
@@ -103,7 +103,7 @@ class CartItem(BaseModel):
 def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     """ """
     global cart_dict
-    cart_dict[cart_id][item_sku] = cart_item
+    cart_dict[cart_id][item_sku] = cart_item.quantity
 
     return "OK"
 
