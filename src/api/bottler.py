a567eb32e5fdcc3e0ef21d2825a5f17800a9707c
BPond4 <bpond@calpoly.edu>
2024-04-21 18:40:12 -0700

    fixed bottler error

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -116,9 +116,7 @@ def get_bottle_plan():
             )
     
     
-    return [
-            potion_list
-        ]
+    return potion_list
 
 if __name__ == "__main__":
     print(get_bottle_plan())
