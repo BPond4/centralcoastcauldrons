891e9de17a9f1b81f6f4d7f7e9f15744003ff112
BPond4 <bpond@calpoly.edu>
2024-04-29 20:00:56 -0700

    catalog fix

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -16,19 +16,19 @@ def get_catalog():
         
         potions = connection.execute(sqlalchemy.text("SELECT id, sku, price, red, green, blue, dark FROM potions")).fetchall()
         for potion in potions:
-            quantity = connection.execute(sqlalchemy.text("SELECT SUM(quantity) FROM potion_ledgers WHERE potion_id = :potion_id"),{"potion_id":potion[0]}).fetchone()[0]
-            
-            if(count<6 and quantity>0):
-                cur_items.append(
-                    {
-                        "sku": potion[1],
-                        "name": potion[1],
-                        "quantity": quantity,
-                        "price": potion[2],
-                        "potion_type": [potion[3], potion[4], potion[5], potion[6]],
-                    }
-                )
-                count+=1
+            quantity = connection.execute(sqlalchemy.text("SELECT SUM(num_potions) FROM potion_ledgers WHERE potion_id = :potion_id"),{"potion_id":potion[0]}).fetchone()[0]
+            if(quantity):
+                if(count<6 and quantity>0):
+                    cur_items.append(
+                        {
+                            "sku": potion[1],
+                            "name": potion[1],
+                            "quantity": quantity,
+                            "price": potion[2],
+                            "potion_type": [potion[3], potion[4], potion[5], potion[6]],
+                        }
+                    )
+                    count+=1
        
 
     
