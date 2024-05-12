489de081d08e2ead04190bef24777d688f87ea38
BPond4 <bpond@calpoly.edu>
2024-05-12 12:35:47 -0700

    barreler update

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -94,7 +94,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         iterations+=1
         for barrel in wholesale_catalog:
             if(red_ml == 0 or (red_ml<=blue_ml and red_ml<=green_ml) or green_barrel_bought or blue_barrel_bought):
-                if((barrel.potion_type[0]>=1) and (barrel.price<=budget) and barrel.ml_per_barrel>=500 and ((barrel.ml_per_barrel+total_ml) <ml_cap)and (not red_barrel_bought)):
+                if((barrel.potion_type[0]>=1) and (barrel.price<=budget) and barrel.ml_per_barrel>500 and ((barrel.ml_per_barrel+total_ml) <ml_cap)and (not red_barrel_bought)):
                     sku = barrel.sku
                     
                     purchase_plan.append( 
@@ -107,7 +107,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                     total_ml+=barrel.ml_per_barrel
                     red_barrel_bought = True
             elif(green_ml == 0 or (green_ml<=blue_ml and green_ml<=red_ml) or red_barrel_bought or blue_barrel_bought):
-                if((barrel.potion_type[1]>=1) and (barrel.price<=budget) and barrel.ml_per_barrel>=500 and ((barrel.ml_per_barrel+total_ml) <ml_cap) and (not green_barrel_bought)):
+                if((barrel.potion_type[1]>=1) and (barrel.price<=budget) and barrel.ml_per_barrel>500 and ((barrel.ml_per_barrel+total_ml) <ml_cap) and (not green_barrel_bought)):
                     sku = barrel.sku
                     
                     purchase_plan.append( 
@@ -120,7 +120,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                     total_ml+=barrel.ml_per_barrel
                     green_barrel_bought = True
             elif(blue_ml == 0 or (blue_ml<=red_ml and blue_ml<=green_ml) or green_barrel_bought or red_barrel_bought):
-                if((barrel.potion_type[2]>=1) and (barrel.price<=budget) and barrel.ml_per_barrel>=500 and ((barrel.ml_per_barrel+total_ml) <ml_cap)and (not blue_barrel_bought)):
+                if((barrel.potion_type[2]>=1) and (barrel.price<=budget) and barrel.ml_per_barrel>500 and ((barrel.ml_per_barrel+total_ml) <ml_cap)and (not blue_barrel_bought)):
                     sku = barrel.sku
                     
                     purchase_plan.append( 
@@ -133,7 +133,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                     total_ml+=barrel.ml_per_barrel
                     blue_barrel_bought = True
             elif(dark_ml == 0 or (dark_ml<=blue_ml and dark_ml<=green_ml)):
-                if((barrel.potion_type[3]>=1) and (barrel.price<=budget) and barrel.ml_per_barrel>=500 and ((barrel.ml_per_barrel+total_ml) <ml_cap)and (not dark_barrel_bought)):
+                if((barrel.potion_type[3]>=1) and (barrel.price<=budget) and barrel.ml_per_barrel>500 and ((barrel.ml_per_barrel+total_ml) <ml_cap)and (not dark_barrel_bought)):
                     sku = barrel.sku
                     
                     purchase_plan.append( 
@@ -145,7 +145,10 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                     budget -= barrel.price
                     total_ml+=barrel.ml_per_barrel
                     dark_barrel_bought = True
-            
+        if red_barrel_bought and green_barrel_bought and blue_barrel_bought:
+            red_barrel_bought = False
+            green_barrel_bought = False
+            blue_barrel_bought = False
  
     print(purchase_plan)
     return purchase_plan
