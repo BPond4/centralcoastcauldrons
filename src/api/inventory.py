2dde519f4ca681fb9610beea365c09c250f0a24a
BPond4 <bpond@calpoly.edu>
2024-05-14 07:20:33 -0700

    capacity increase

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -63,7 +63,7 @@ def get_capacity_plan():
     print(f"Buying {potion_increase} potion capacity and {ml_increase} ml capacity")
     return {
         "potion_capacity": potion_increase,
-        "ml_capacity": ml_increase
+        "ml_capacity": 2
         }
 
 class CapacityPurchase(BaseModel):
